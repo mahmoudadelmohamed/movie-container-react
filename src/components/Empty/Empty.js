22acb8723b95a7e7495489ab4f27bd06aa1f1071
Mahmoud Adel Mohamed <mahmoudadelfcis@gmail.com>
2020-04-27 23:57:22 +0200

    push

--- src/components/Empty/Empty.js
+++ src/components/Empty/Empty.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import notFoundImage from '../../assets/images/not-found.png';
+import notFoundImage from '../../assets/images/not-found1.gif';
+import "./Empty.scss";
 const Empty = ({ title, title_name }) => {
   return (
-    <div className="text-center d-flex justify-content-center align-items-center h-100 flex-column">
+    <div className="container ">
+    <div className="text-center d-flex justify-content-center align-items-center h-100 flex-column ">
       <h1 className="h1-2-light title_uppercase">Sorry!</h1>
-      <h2 className="h2-light title_uppercase">{title} {title_name}</h2>
-      <img src={notFoundImage} alt="notFoundImage" className="w-50 mb-4"/>
+      <h2 className="h2-light title_uppercase mb-3">{title} {title_name}</h2>
+      <img src={notFoundImage} alt="notFoundImage" className="mb-4 w-50"/>
+    </div>
       {/*
         <Link to="/" style={{textDecoration: 'none'}}>
         <Buttons
